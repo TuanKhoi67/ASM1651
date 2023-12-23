1b86cb221213727c65e6db225fbce40746d45cf7
TKhoi2 <153786779+TuanKhoi67@users.noreply.github.com>
2023-12-23 15:50:35 +0700

    Up

--- ASM1651/app.js
+++ ASM1651/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var toyRouter = require('./routes/toy');
+var brandRouter = require('./routes/brand');
 
 var app = express();
 
